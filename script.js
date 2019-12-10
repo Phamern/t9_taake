4438345aeb754bb841d9583f58f5c82a8c16575e
Eivind Madsen Pham <eivindmpham@gmail.com>
2019-12-10 16:32:52 +0100

    Fixed origin file. social anxiety page and paranoia is mostly done

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ document.querySelectorAll("nav a").forEach(e => {
       break;
       case "Borderline":
         document.querySelector('.symptoms').classList.add('schizoSympt');
-      default:
+      break;
+      case "Histrionic":
+        document.querySelector('.symptoms').classList.add('schizoSympt');
+      break;
+      case "Narcissistic":
+        document.querySelector('.symptoms').classList.add('schizoSympt');
       break;
     }
   })
@@ -17,6 +22,12 @@ document.querySelectorAll("nav a").forEach(e => {
         document.querySelector('.symptoms').classList.remove('schizoSympt');
       break;
       case "Borderline":
+        document.querySelector('.symptoms').classList.remove('schizoSympt');
+      break;
+      case "Histrionic":
+        document.querySelector('.symptoms').classList.remove('schizoSympt');
+      break;
+      case "Narcissistic":
         document.querySelector('.symptoms').classList.remove('schizoSympt');
       break;
       default:
@@ -34,33 +45,32 @@ function setup() {
 
   // load images
   let pic1 = localStorage.getItem('pic0');
-  if(pic1){
-    showSnap(pic1);
-  }
-  let pic2 = localStorage.getItem('pic1');
-  if(pic2){
-    showSnap(pic2);
-  }
-  let pic3 = localStorage.getItem('pic2');
-  if(pic3){
-    showSnap(pic3);
-  }
-  let pic4 = localStorage.getItem('pic3');
-  if(pic4){
-    showSnap(pic4);
-  }
+  // if(pic1){
+  // //   showSnap(pic1);
+  // // }
+  // let pic2 = localStorage.getItem('pic1');
+  // // if(pic2){
+  // //   showSnap(pic2);
+  // // }
+  // let pic3 = localStorage.getItem('pic2');
+  // // if(pic3){
+  // //   showSnap(pic3);
+  // // }
+  // let pic4 = localStorage.getItem('pic3');
+  // // if(pic4){
+  // //   showSnap(pic4);
+  // // }
 
   if(!pic1) {
     // capture images
-      createCanvas(width, height);
-      video = createCapture(VIDEO, function(event) {
-      snapInterval = setInterval(takesnap, 1000);
+    createCanvas(width, height);
+    video = createCapture(VIDEO, function(event) {
+    snapInterval = setInterval(takesnap, 1000);
     });
     video.size(width, height);
     video.hide();
   }
 };
-
 
 function takesnap() {
   if(snapCount < 5) {
@@ -68,8 +78,6 @@ function takesnap() {
     let snap = video.get();
     var myImage = snap.canvas.toDataURL("image/png"); 
     localStorage.setItem('pic'+ snapCount, myImage);
-
-    //showSnap(myImage);
   } else {
     clearInterval(snapInterval);
     video.remove();
